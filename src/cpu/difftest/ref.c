a445c9e8fd34c968e55c89da3c124d2d786f805a
Furinaaa <1145676148@qq.com>
2025-03-03 00:23:51 +0800

    Update ref.c

--- src/cpu/difftest/ref.c
+++ src/cpu/difftest/ref.c
@@ -30,32 +30,38 @@
 // 在DUT host memory的`buf`和REF guest memory的`addr`之间拷贝`n`字节,
 // `direction`指定拷贝的方向, `DIFFTEST_TO_DUT`表示往DUT拷贝, `DIFFTEST_TO_REF`表示往REF拷贝
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-  // if(direction == DIFFTEST_TO_REF){
-  //   memcpy(guest_to_host(addr), buf, n);
-  // }else if(direction == DIFFTEST_TO_DUT){
-  //   memcpy(buf, guest_to_host(addr), n);
-  // }else{
-  //   printf("Invaild direction in difftest_memcpy\n");
-  //   assert(0);  
-  // }
-  assert(0);
+  if(direction == DIFFTEST_TO_REF){
+    printf("addr->%lx  imm:%lx\n", addr,(word_t)buf);
+    memcpy(guest_to_host(addr), buf, n);
+  }else if(direction == DIFFTEST_TO_DUT){
+    // memcpy(buf, guest_to_host(addr), n);
+    paddr_read(addr,n);
+  }else{
+    printf("Invaild direction in difftest_memcpy\n");
+    assert(0);  
+  }
 }
 // `direction`为`DIFFTEST_TO_DUT`时, 获取REF的寄存器状态到`dut`;
 // `direction`为`DIFFTEST_TO_REF`时, 设置REF的寄存器状态为`dut`;
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-  // if(direction==DIFFTEST_TO_REF){
-  //   memcpy(&ref_regs, dut, sizeof(ref_regs));
-  // }else if(direction==DIFFTEST_TO_DUT){
-  //   memcpy(dut,&ref_regs,sizeof(ref_regs));
-  // }else{
-  //   printf("Invaild direction in difftest_regcpy\n");
-  //   assert(0);
-  // }
-  assert(0);
+  if(direction == DIFFTEST_TO_REF){
+    for(uint16_t i=0;i<32;i++){
+      cpu.gpr[i]=dut->gpr[i];
+    }
+    cpu.pc=dut->pc;
+  }else if(direction == DIFFTEST_TO_DUT){
+    for(uint16_t i=0;i<32;i++){
+      dut->gpr[i]=cpu.gpr[i];
+    }
+    dut->pc=cpu.pc;
+  }else{
+    printf("Invaild direction in difftest_regcpy\n");
+    assert(0);
+  }
 }
 // 让REF执行`n`条指令
 __EXPORT void difftest_exec(uint64_t n) {
-  assert(0);
+  cpu_exec(n);
 }
 
 __EXPORT void difftest_raise_intr(word_t NO) {
@@ -67,4 +73,6 @@ __EXPORT void difftest_init(int port) {
   init_mem();
   /* Perform ISA dependent initialization. */
   init_isa();
+  cpu.gpr[0]=0;
+  cpu.pc=RESET_VECTOR;
 }
